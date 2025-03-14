5bd0972934b2e676f522249379924dbf4d50f8cc
ShailySarker <shailysarker99@gmail.com>
2025-03-14 14:51:12 +0600

    models added

---
+++ models/Coupon.js
@@ -0,0 +1,11 @@
+const mongoose = required ("mongoose");
+
+const couponSchema = new mongoose.Schema({
+    code: { type: String, required: true, unique: true },
+    claimedBy: { type: mongoose.Schema.Types.ObjectId, ref: "User", default: null },
+    claimedAt: { type: Date, default: null },
+});
+
+const Coupon = mongoose.model('Coupon', couponSchema);
+
+module.exports = Coupon;
---
+++ models/User.js
@@ -0,0 +1,10 @@
+const mongoose = required ("mongoose");
+
+const userSchema = new mongoose.Schema({
+    ipAddress: { type: String, required: true, unique: true },
+    lastClaimed: { type: Date, default: null },
+});
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
